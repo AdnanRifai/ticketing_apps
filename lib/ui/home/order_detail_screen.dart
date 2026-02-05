54f8a5eb60ae52c3fa830a724ead5fde05dbfee3
AdnanRifai <bakhrifamily01@gmail.com>
2026-02-05 11:06:19 +0300

    feat: add payment method button component and integrate into order detail screen

--- lib/ui/home/order_detail_screen.dart
+++ lib/ui/home/order_detail_screen.dart
@@ -5,6 +5,7 @@ import 'package:ticketing_apps/core/components/space.dart';
 import 'package:ticketing_apps/core/contstans/colors.dart';
 import 'package:ticketing_apps/core/extentions/build_context_ext.dart';
 import 'package:ticketing_apps/core/extentions/idr_currency.dart';
+import 'package:ticketing_apps/core/widgets/payment_method_button.dart';
 import 'package:ticketing_apps/ui/home/model/product__model.dart';
 
 class OrderDetailScreen extends StatelessWidget {
@@ -13,6 +14,8 @@ class OrderDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int payemntButtonIndex = 0;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Detail Pesanan'),
@@ -73,6 +76,91 @@ class OrderDetailScreen extends StatelessWidget {
         },
         separatorBuilder: (context, index) => SpaceHeight(20),
         itemCount: products.length,
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          StatefulBuilder(
+            builder: (context, setState) {
+              return Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: PaymentMethodButton(
+                        iconPath: Assets.icons.payment.qris.path,
+                        label: "QRIS",
+                        isActive: payemntButtonIndex == 0,
+                        onPressed: () => setState(() => payemntButtonIndex = 0),
+                      ),
+                    ),
+                    SpaceWidth(20),
+                    Expanded(
+                      child: PaymentMethodButton(
+                        iconPath: Assets.icons.payment.tunai.path,
+                        label: "Tunai",
+                        isActive: payemntButtonIndex == 1,
+                        onPressed: () => setState(() => payemntButtonIndex = 1),
+                      ),
+                    ),
+                    SpaceWidth(20),
+                    Expanded(
+                      child: PaymentMethodButton(
+                        iconPath: Assets.icons.payment.transfer.path,
+                        label: "Transfer",
+                        isActive: payemntButtonIndex == 2,
+                        onPressed: () => setState(() => payemntButtonIndex = 2),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+          SpaceHeight(27),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 30,
+                  spreadRadius: 0,
+                  offset: Offset(0, -2),
+                  color: AppColors.black.withOpacity(0 / 100),
+                  blurStyle: BlurStyle.outer,
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Order Summary",
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                      SpaceHeight(4),
+                      Text(
+                        200000.currencyFormatRp,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Button.filled(onPressed: () {}, label: "Proccess"),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
